c723d9a1a6ce49e98a5f450408fcaa9bb9aeefce
Sabina <sabinabeisenova@gmail.com>
2022-07-07 16:15:54 +0600

    initialization of project

--- courses_app/views.py
+++ courses_app/views.py
@@ -1,10 +1,27 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from courses_app.models import Course
 from courses_app.serializers import CourseSerializers
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
+
+
+class CourseFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Course
+        fields = ['price']
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     '''предоставляет для фронта информацию о курсах'''
     queryset = Course.objects.all()
     serializer_class = CourseSerializers
-
+    lookup_field = 'pk'
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['course']
+    search_fields = ['name', 'duration', 'price']
+    ordering_fields = ['name', 'duration', 'price']
+    filterset_class = CourseFilter
--- students_app/views.py
+++ students_app/views.py
@@ -1,9 +1,16 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from students_app.models import Student
 from students_app.serializers import StudentSerializers
 from rest_framework import viewsets
 
-
 class StudentViewSet(viewsets.ReadOnlyModelViewSet):
+
     '''предоставляет для фронта информацию о курсах'''
     queryset = Student.objects.all()
     serializer_class = StudentSerializers
+    lookup_field = 'pk'
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['student']
+    search_fields = ['first_name', 'last_name']
+    ordering_fields = ['first_name', 'last_name']

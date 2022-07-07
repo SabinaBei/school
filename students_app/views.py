3859fdd3e1d6ed43a5c5973c780dc1c88fb66fc7
Sabina <sabinabeisenova@gmail.com>
2022-07-07 16:27:34 +0600

    initialization of project

--- students_app/views.py
+++ students_app/views.py
@@ -5,12 +5,11 @@ from students_app.serializers import StudentSerializers
 from rest_framework import viewsets
 
 class StudentViewSet(viewsets.ReadOnlyModelViewSet):
-
     '''предоставляет для фронта информацию о курсах'''
     queryset = Student.objects.all()
     serializer_class = StudentSerializers
     lookup_field = 'pk'
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['student']
+    filterset_fields = ['course']
     search_fields = ['first_name', 'last_name']
     ordering_fields = ['first_name', 'last_name']
